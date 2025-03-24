2663b4095ab14071fea91b4cc6f081843ab50935
ArielleAlayon <ariellealayon05@gmail.com>
2025-03-24 00:33:50 -0700

    Functional Activity 1

---
+++ Activities/script.js
@@ -0,0 +1,12 @@
+function whatshalliwear (temperature) {
+    if (temperature < 60) {
+        console.log ("wear a jacket");
+    }else if (temp < 70) {
+        console.log ("wear a sweater")
+    }else {
+        console.log("wear a t_shirt");
+    }
+}
+whatshalliwear(50)
+whatshalliwear(80)
+whatshalliwear(60)
